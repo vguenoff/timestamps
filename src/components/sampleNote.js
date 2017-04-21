fc790264e9d0b2304be6ced272e328f3211c9380
vg <vg@Vassils-MacBook-Air.local>
2017-04-21 14:16:42 +0300

    welcome note edit

--- src/components/sampleNote.js
+++ src/components/sampleNote.js
@@ -1,7 +1,7 @@
 module.exports = {
   note1: {
     timestamp: Date(),
-    title: 'Welkome',
-    content: 'Thanks for trying TIMESTAMPS. :-) You can add, edit and delete notes here. \nPlus all your data is saved in your device\'s local storage!',
+    title: 'Welcome',
+    content: 'Thanks for trying TIMESTAMPS. :-) You can add, edit and delete notes here. \nPlus all your data is saved in your browser\'s local storage!',
   }
 };
